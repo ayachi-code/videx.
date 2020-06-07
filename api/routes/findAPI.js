70ee625c073c1f0d7c56e24a2385210afdaf1858
Bilal ayachi <bilal.ayachi@hetmml.nl>
2020-06-07 18:51:28 +0200

    Gebruik imdb db en error handling voor findAPI door te checken naar empty array

--- api/routes/findAPI.js
+++ api/routes/findAPI.js
@@ -14,16 +14,38 @@ router.get("/:movie", function(req, res, next) {
 
 router.get("/:movie", function(req, res, next) {
   fetch("https://api.themoviedb.org/3/search/movie?api_key=ac5a895c793b78f0116f0b63423fc115&language=en-US&query=" + req.params.movie + "&page=1&include_adult=false")
-  .then(res => res.json())
+  .then(response => response.json())
   .then((data) => {
     let movieinfo = {
       "name": []
     }
-    for (let i = 0; i < 5; i++) {
-      movieinfo.name.push(data.results[i].original_title)
+    let status = {
+      exist: false
     }
-      //Sent data to client...
+
+    if (data.results === undefined || data.results.length == 0) {
+      res.send(status)
+
+    } else {
+      for (let i = 0; i < 5; i++) {
+        movieinfo.name.push(data.results[i].original_title)
+      }
       res.send(movieinfo)
+    }
+
+    /*
+    for (let i = 0; i < 5; i++) {
+      //movieinfo.name.push(data.results[i].original_title)
+      if (data.results[i].original_title === undefined) {
+        console.log("bestaat niet .....")
+      } else {
+        console.log("asdasd")
+      }
+    }
+    */
+      //Sent data to client...
+      //console.log(data)
+      //res.send(movieinfo)
   })
 });
 
--- api/routes/findinfo.js
+++ api/routes/findinfo.js
@@ -3,9 +3,38 @@ var router = express.Router();
 var fetch = require("node-fetch")
 
 
-router.get("/:info", function(req, res, next) {
+router.get("/:info",function(req, res, next) {
+
+  const url = "http://www.omdbapi.com/?t=" + req.params.info + "&apikey=adb375bd"
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    let movie_info = {
+      name: [],
+      language: [],
+      Metascore: [],
+      overview: [],
+      release_date: []
+  }  
+  movie_info.name.push(data.Title);
+  movie_info.language.push(data.Language);
+  movie_info.Metascore.push(data.Metascore);
+  movie_info.overview.push(data.Plot);
+  movie_info.release_date.push(data.Released);
+  
+  console.log(data)
+
+  res.send(movie_info)
+  
+  })
+
+
+  /*
+  console.log("https://api.themoviedb.org/3/search/movie?api_key=ac5a895c793b78f0116f0b63423fc115&language=en-US&query=" + req.params.info + "&page=1&include_adult=false")
   fetch("https://api.themoviedb.org/3/search/movie?api_key=ac5a895c793b78f0116f0b63423fc115&language=en-US&query=" + req.params.info + "&page=1&include_adult=false")
-  .then(res => res.json())
+  .then(response => console.log(response.status))
+  */ 
+  /*
   .then((data) => { 
     let movie_info = {
         name: [],
@@ -14,16 +43,18 @@ router.get("/:info", function(req, res, next) {
         overview: [],
         release_date: []
     }  
-    
+  
     movie_info.name.push("Title: " + data.results[0].original_title)
     movie_info.language.push("Language: " + data.results[0].original_language)      
     movie_info.vote_average.push("Vote average " + data.results[0].vote_average)      
     movie_info.overview.push(data.results[0].overview)      
-    movie_info.release_date.push("Release date: " + data.results[0].release_date)              
+    movie_info.release_date.push("Release date: " + data.results[0].release_date)              npm 
 
     //Sent data to client...
     res.send(movie_info)
+    
   })
+  */
 });
 
 module.exports = router;
